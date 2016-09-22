5efee5fcd3714ebb83116908edd80dbc990d5594
Alison <alisonhmullin@gmail.com>
2016-09-22 16:08:53 -0400

    complete create element lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Code React element here
+
+const Title = React.createElement('h1', {}, 'An Awesome Person')
+
+const Tagline = React.createElement('p', {}, 'Who is learning React')
+
+const Interests = React.createElement('ul', {className: 'me_interests'}, [
+  React.createElement('li', {}, 'JavaScript'),
+  React.createElement('li', {}, 'React'),
+  React.createElement('li', {}, 'Movies'),
+  React.createElement('li', {}, 'Ice cream')
+  ])
+
+const meInReact = React.createElement('div', {className: 'me'}, [Title, Tagline, Interests]);
